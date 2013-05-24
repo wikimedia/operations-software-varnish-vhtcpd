de5295cedc9c3135906a91ab37fe014deee4eb7e
Brandon Black <bblack@wikimedia.org>
2013-05-24 13:22:54 +0000

    Debian stuff

---
+++ debian/vhtcpd.default.ex
@@ -0,0 +1,10 @@
+# Defaults for vhtcpd initscript
+# sourced by /etc/init.d/vhtcpd
+# installed at /etc/default/vhtcpd by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
